8f058c6f51604c6fbf830e121d8efc70b8d82d22
E490X <thanosminati@gmail.com>
2025-06-16 21:13:51 +0530

    Updated badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,9 +9,9 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground shadow hover:bg-[#E0A784]",
+          "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-transparent bg-secondary text-secondary-foreground hover:[#E0A784]",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
         outline: "text-foreground",
