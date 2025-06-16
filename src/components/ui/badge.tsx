8cc8d3b1925e0cd484c7136b9e88952c38a24879
E490X <thanosminati@gmail.com>
2025-06-16 21:15:23 +0530

    Updated badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,7 +9,7 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground shadow hover:[rgba(224,167,132,0.45)]",
+          "border-transparent bg-primary text-primary-foreground shadow hover:[rgba(224,167,132,0.85)]",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:[bg-secondary/80]",
         destructive:
